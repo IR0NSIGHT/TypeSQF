65f5534ed92c6108ac0cf8cc366f7c40f65abd02
IR0NSIGHT <irnsght@googlemail.com>
2023-06-30 15:08:44 +0200

    use sheband to use node env to index file

--- src/cli_index.ts
+++ src/cli_index.ts
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import { Command } from "commander";
 import { addSplitBlobAction } from "./CLI_Operations/splitBlobOperation";
 import { addBuildCfgFunctionsAction } from "./CLI_Operations/buildCfgFunctionsFile";
